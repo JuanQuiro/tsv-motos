8ffb85f15b3102afb427b0b8b825ea6f3c88d09e
JuanQuiro <juanquirozsana@gmail.com>
2024-01-29 01:12:22 -0400

    feat: :sparkles: actualizar carrusel

--- components/Botonera.tsx
+++ components/Botonera.tsx
@@ -81,11 +81,11 @@ const ComponenteEspecificoTres = () => {
   const ComponenteEspecificoSiete = () => {
     return <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
       <Carousel>
-        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+        <img src="/1Rojo.jpg" alt="..." />
+        <img src="/2Rojo.png" alt="..." />
+        <img src="/3Rojo.jpg" alt="..." />
+        <img src="/4Rojo.jpg" alt="..." />
+        <img src="/5Rojo.png" alt="..." />
       </Carousel>
     </div>
   
--- components/Botonera2.tsx
+++ components/Botonera2.tsx
@@ -65,11 +65,11 @@ const ComponenteEspecificoTres = () => {
   const ComponenteEspecificoSiete = () => {
     return <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
       <Carousel>
-        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+        <img src="/1Blanco.png" alt="..." />
+        <img src="/2Blanco.jpg" alt="..." />
+        <img src="/3Blanco.jpg" alt="..." />
+        <img src="/4Blanco.jpg" alt="..." />
+        <img src="/5Blanco.jpg" alt="..." />
       </Carousel>
     </div>
   
