b9e1376b4c050c0c4afaf2f940a5afa7360f33c1
juanquiro <juanquirozsanchez@hotmail.com>
2024-02-06 09:19:36 +0000

    5

--- pages/api/enviarForm.js
+++ pages/api/enviarForm.js
@@ -1,22 +1,22 @@
-import { PrismaClient } from '@prisma/client'
+import prisma from '../../lib/prisma'
 
-const prisma = new PrismaClient()
-
-prisma.user.create({
-  data: {
-    name: 'Alice',
-    email: 'alice@prisma.io',
-    Ingresos: '500',
-    Yummy: true,
-    Extranjero: true,
-    Cedula: 'sasasa',
-    PrimerNombre: 'sasasa',
-    SegundoNombre: 'sasasas',
-    PrimerApellido: 'asasas',
-    SegundoApellido: 'saasa',
-    Dirrecion: 'asa',
-    message: 'as',
-    Pais: 'paris',
-    subject: 'sasa'
-  }
-})
+export default async function handle() {
+  await prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alice@prisma.io',
+      Ingresos: '500',
+      Yummy: true,
+      Extranjero: true,
+      Cedula: 'sasasa',
+      PrimerNombre: 'sasasa',
+      SegundoNombre: 'sasasas',
+      PrimerApellido: 'asasas',
+      SegundoApellido: 'saasa',
+      Dirrecion: 'asa',
+      message: 'as',
+      Pais: 'paris',
+      subject: 'sasa'
+    }
+  })
+}
