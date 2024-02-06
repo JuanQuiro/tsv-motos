06490b7a873f3d1f25c73e0b62605dcbf50b061a
juanquiro <juanquirozsanchez@hotmail.com>
2024-02-06 08:59:32 +0000

    2121

--- pages/api/enviarForm.js
+++ pages/api/enviarForm.js
@@ -1,40 +1,22 @@
 import { PrismaClient } from '@prisma/client'
 
-const handler = async (req, res) => {
-  const prisma = new PrismaClient()
-  if (req.method === 'POST') {
-    const data = req.body
-    if (!data || !data.name || !data.email || !data.subject || !data.message) {
-      return res.status(400).send({ message: 'Bad request' })
-    }
+const prisma = new PrismaClient()
 
-    try {
-      await prisma.user.create({
-        data: {
-          name: 'Alice',
-          email: 'alice@prisma.io',
-          Ingresos: '500',
-          Yummy: true,
-          Extranjero: true,
-          Cedula: 'sasasa',
-          PrimerNombre: 'sasasa',
-          SegundoNombre: 'sasasas',
-          PrimerApellido: 'asasas',
-          SegundoApellido: 'saasa',
-          Dirrecion: 'asa',
-          message: 'as',
-          Pais: 'paris',
-          subject: 'sasa'
-        }
-      })
-
-      return res.status(200).json({ success: true })
-    } catch (err) {
-      console.log(err)
-      return res.status(400).json({ message: err.message })
-    }
+prisma.user.create({
+  data: {
+    name: 'Alice',
+    email: 'alice@prisma.io',
+    Ingresos: '500',
+    Yummy: true,
+    Extranjero: true,
+    Cedula: 'sasasa',
+    PrimerNombre: 'sasasa',
+    SegundoNombre: 'sasasas',
+    PrimerApellido: 'asasas',
+    SegundoApellido: 'saasa',
+    Dirrecion: 'asa',
+    message: 'as',
+    Pais: 'paris',
+    subject: 'sasa'
   }
-  return res.status(400).json({ message: 'Bad request' })
-}
-
-export default handler
+})
