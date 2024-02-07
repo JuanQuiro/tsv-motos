7efc51bfe8d66fa6c9e4dea126cbe9bb901d1ff8
juanquiro <juanquirozsanchez@hotmail.com>
2024-02-07 05:16:43 +0000

    asasa

--- pages/api/enviarForm.js
+++ pages/api/enviarForm.js
@@ -1,22 +1,22 @@
 import prisma from '../../lib/prisma'
 
-export default async function handle(informacion) {
-  await prisma.user.create({
+export default async function handle(info) {
+  const result = await prisma.user.create({
     data: {
-      name: informacion.name,
-      email: informacion.email,
-      Ingresos: informacion.Ingresos,
-      Yummy: informacion.Yummy,
-      Extranjero: informacion.Extranjero,
-      Cedula: informacion.Cedula,
-      PrimerNombre: informacion.PrimerNombre,
-      SegundoNombre: informacion.SegundoNombre,
-      PrimerApellido: informacion.PrimerApellido,
-      SegundoApellido: informacion.SegundoApellido,
-      Dirrecion: informacion.Dirrecion,
-      message: informacion.message,
-      Pais: informacion.Pais,
-      subject: informacion.subject
+      name: `${info.name}`,
+      email: `${info.email}`,
+      Ingresos: `${info.Ingresos}`,
+      Yummy: true,
+      Extranjero: true,
+      Cedula: `${info.Cedula}`,
+      PrimerNombre: `${info.PrimerNombre}`,
+      SegundoNombre: `${info.SegundoNombre}`,
+      PrimerApellido: `${info.PrimerApellido}`,
+      SegundoApellido: `${info.SegundoApellido}`,
+      Dirrecion: `${info.Dirrecion}`,
+      message: `${info.message}`,
+      Pais: 'paris',
+      subject: 'sasa'
     }
   })
   return res.status(200).json({ success: true })
