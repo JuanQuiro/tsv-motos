a39664760f025418cd01451d8049ae25b0bf8e6f
juanquiro <juanquirozsanchez@hotmail.com>
2024-02-07 04:28:45 +0000

    98

--- pages/api/enviarForm.js
+++ pages/api/enviarForm.js
@@ -1,22 +1,22 @@
+import { data } from 'autoprefixer'
 import prisma from '../../lib/prisma'
 
 export default async function handle() {
   const result = await prisma.user.create({
     data: {
-      name: 'Alice',
-      email: 'alice@prisma.io',
-      Ingresos: '500',
-      Yummy: true,
-      Extranjero: true,
-      Cedula: 'sasasa',
-      PrimerNombre: 'sasasa',
-      SegundoNombre: 'sasasas',
-      PrimerApellido: 'asasas',
-      SegundoApellido: 'saasa',
-      Dirrecion: 'asa',
-      message: 'as',
-      Pais: 'paris',
-      subject: 'sasa'
+      name: data.name,
+      email: data.email,
+      Ingresos: data.Ingresos,
+      Extranjero: data.Extranjero,
+      Cedula: data.Cedula,
+      PrimerNombre: data.PrimerNombre,
+      SegundoNombre: data.SegundoNombre,
+      PrimerApellido: data.PrimerApellido,
+      SegundoApellido: data.SegundoApellido,
+      Dirrecion: data.Dirrecion,
+      message: data.message,
+      Pais: data.Pais,
+      subject: data.subject
     }
   })
         return res.status(200).json({ success: true })
