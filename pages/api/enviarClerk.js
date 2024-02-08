f707718272e0c960c235e75a414415618ea47cb1
juanquiro <juanquirozsanchez@hotmail.com>
2024-02-08 04:29:50 +0000

    7o7o

--- pages/api/enviarClerk.js
+++ pages/api/enviarClerk.js
@@ -8,12 +8,12 @@ const handler = async (req, res) => {
       await prisma.clerk.create({
         data: {
           id_clerk: `${info.id}`,
-          number: `${info.number}`,
-          gmail: `${info.gmail}`,
-          img: `${info.img}`,
+          number: `${info.primaryPhoneNumber.phoneNumber}`,
+          gmail: `${info.externalAccounts[0].emailAddress}`,
+          img: `${info.imageUrl}`,
           username: `${info.username}`,
-          first_name: `${info.first_name}`,
-          last_name: `${info.last_name}`
+          first_name: `${info.firstName}`,
+          last_name: `${info.lastName}`
         }
       })
       return res.status(200).json({ success: true })
