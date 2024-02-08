3ace27ab225a99e3c213bd78735485ad0cdce1fc
juanquiro <juanquirozsanchez@hotmail.com>
2024-02-08 05:25:53 +0000

    |21

--- pages/api/usuarioCorreo.js
+++ pages/api/usuarioCorreo.js
@@ -1,26 +1,24 @@
-import { mailOptions, transporter } from "../../config/nodemailer";
+import { mailOptions, transporter } from '../../config/nodemailer'
 
 const CONTACT_MESSAGE_FIELDS = {
-  name: "Juan",
-  email: "juanquirozsana@gmail.com",
-  subject: "juanquirozsana@gmail.com",
-  message: "EMAIL",
-};
-  
+  name: 'Juan',
+  email: 'juanquirozsana@gmail.com',
+  subject: 'juanquirozsana@gmail.com',
+  message: 'EMAIL'
+}
 
-const generateEmailContent = (data) => {
-
-  const currentDate = new Date();
-  const currentDateTime = currentDate.toLocaleString();
+const generateEmailContent = data => {
+  const currentDate = new Date()
+  const currentDateTime = currentDate.toLocaleString()
 
   const stringData = Object.entries(data).reduce(
     (str, [key, val]) =>
       (str += `${CONTACT_MESSAGE_FIELDS[key]}: \n${val} \n \n`),
-    ""
-  );
+    ''
+  )
   const htmlData = Object.entries(data).reduce((str, [key, val]) => {
-    return (str += `<h3 class="form-heading" align="left">${CONTACT_MESSAGE_FIELDS[key]}</h3><p class="form-answer" align="left">${val}</p>`);
-  }, "");
+    return (str += `<h3 class="form-heading" align="left">${CONTACT_MESSAGE_FIELDS[key]}</h3><p class="form-answer" align="left">${val}</p>`)
+  }, '')
 
   return {
     text: stringData,
@@ -35,40 +33,34 @@ const generateEmailContent = (data) => {
         <p>Estimado Equipo de Recursos Humanos de Yummy,</p>
         <p>Es un placer informarles que un nuevo usuario ha completado el proceso de aplicación al crédito de TVS Finances. A continuación, los detalles del solicitante:</p>
         <ul>
-            <li>Nombre: ${data.PrimerNombre}</li>
-            <li>Apellido: ${data-PrimerAprellido}</li>
             <li>Número de Identificación: GENERANDOSE</li>
-            <li>Fecha y Hora de Solicitud: ${currentDateTime}</li>
         </ul>
         <p>Para revisar la información completa y proceder con la verificación, por favor haga clic <a href="enlace al dashboard de administrador de Yummy">aquí</a>.</p>
         <p>Gracias por su pronta atención a este asunto.</p>
         <p>Solicitud Número: GENERANDOSE</p>
         <p>*Este es un mensaje automático, por favor no responder. En caso de inconvenientes lo invitamos a ponerse en contacto con nuestro servicio técnico haciendo clic <a href="Link para enviar correo a servicio técnico">aquí</a>.</p>
     </body>
-    </html>`,
-  };
-};
+    </html>`
+  }
+}
 
 const handler = async (req, res) => {
-  if (req.method === "POST") {
-    const data = req.body;
-    if (!data || !data.name || !data.email || !data.subject || !data.message) {
-      return res.status(400).send({ message: "Bad request" });
-    }
+  if (req.method === 'POST') {
+    const data = req.body
 
     try {
       await transporter.sendMail({
         ...mailOptions,
         ...generateEmailContent(data),
-        subject: 'Nueva Solicitud de Crédito - TVS Finances',
-      });
+        subject: 'Nueva Solicitud de Crédito - TVS Finances'
+      })
 
-      return res.status(200).json({ success: true });
+      return res.status(200).json({ success: true })
     } catch (err) {
-      console.log(err);
-      return res.status(400).json({ message: err.message });
+      console.log(err)
+      return res.status(400).json({ message: err.message })
     }
   }
-  return res.status(400).json({ message: "Bad request" });
-};
-export default handler;
+  return res.status(400).json({ message: 'Bad request' })
+}
+export default handler
