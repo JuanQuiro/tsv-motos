148254e79afdf833f5bb34198f150815cbe167b6
Dreyz <54697159+JuanQuiro@users.noreply.github.com>
2024-02-13 09:48:14 +0000

    454

--- app/credito/page.tsx
+++ app/credito/page.tsx
@@ -1,6 +1,21 @@
 import Form from '@/components/form'
+import { PrismaClient } from '@prisma/client';
+import { auth } from '@clerk/nextjs';
 
-export default function Home() {
+const prisma: PrismaClient = new PrismaClient();
+
+export default async function Home() {
+  const { userId } : { userId: string | null } = auth();
+  const allData = await prisma.clerk.findMany({});
+  
+
+  
+  const filteredData = allData.filter(data => data.id_clerk === 'user_2apgrkYwKNabuAuhnbIax9bojTy'); 
+  
+  console.log(filteredData);
+  
+  
+
   return (
     <section className='py-24'>
       <div className='container'>
