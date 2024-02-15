9f86820df97614b660633368c9f7ef57a03ec6e9
Dreyz <54697159+JuanQuiro@users.noreply.github.com>
2024-02-15 09:00:11 +0000

    asasas

--- app/documentos/page.tsx
+++ app/documentos/page.tsx
@@ -7,7 +7,6 @@ const prisma = new PrismaClient()
 const App =  async () => {
   const allData = await prisma.clerk.findMany({});
 
-  if (allData[0]?.estado_formulario == 'Formulario') return redirect('/documentos?formulario=true')
   if (allData[0]?.estado_formulario == 'Finalizado') return redirect('/dashoard-tvs')
 
   return (
