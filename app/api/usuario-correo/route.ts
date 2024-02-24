a10850c45af87bfa90e9397a5ad81131ab2b1dc1
dreyz37 <136238383+dreyz37@users.noreply.github.com>
2024-02-24 08:05:14 +0000

    cambios en usuario-correo

--- app/api/usuario-correo/route.ts
+++ app/api/usuario-correo/route.ts
@@ -1,8 +1,28 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 import { NextResponse } from 'next/server';
 
-export async function POST(req: Request) {
-  
-    return NextResponse.json({ msj: 'HOLA', status : 500, req });
-  
-};
+const prisma = new PrismaClient();
+
+export default async function POST(req: Request) {
+  if (req.method !== 'POST') {
+    return NextResponse.json({ msj: 'Mal no es POST', status : 404 });
+
+  }
+
+  try {
+    const { id_clerk, gmail, iniciando } = await req.body as any;
+
+    const createdCorreo = await prisma.correo_iniciando_estimacion.create({
+      data: {
+        id_clerk,
+        gmail,
+        iniciando,
+      },
+    });
+
+    return NextResponse.json({ msj: 'Creado con exito', status : 201 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ msj: 'Creado con exito', status : 201 });
+  }
+}
