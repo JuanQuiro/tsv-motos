fce671a56d6fc1e4d6768009e3f6b05fad724e39
dreyz37 <136238383+dreyz37@users.noreply.github.com>
2024-02-24 07:10:58 +0000

    validacion try cath

--- app/api/validacionyummy/route.ts
+++ app/api/validacionyummy/route.ts
@@ -1,32 +1,38 @@
 import { PrismaClient } from "@prisma/client";
-import { NextResponse } from 'next/server'
-
-
+import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
-  console.log('sasas');
+  try {
+    const prisma = new PrismaClient();
+    const token = await req.json() as any;
+    console.log(token);
 
-  const prisma = new PrismaClient()
-  const token = await req.json() as any;
-  console.log(token);
+    const serverToken = "123";
 
-  const serverToken = "123";
-
-  if (token.pass === serverToken) {
-    const prismaClerk = async () => {
-      await prisma.admin_yummy.create({
-        data: {
-          autorizacion: true,
-          id_clerk: token.id || 'ERROR'
+    if (token.pass === serverToken) {
+      const prismaClerk = async () => {
+        try {
+          await prisma.admin_yummy.create({
+            data: {
+              autorizacion: true,
+              id_clerk: token.id || 'ERROR'
+            }
+          });
+        } catch (error) {
+          console.error(error);
+          return NextResponse.json({ msj: 'Mal no cargo prisma', status : 404 });
         }
-      })
+      };
+
+      await prismaClerk();
+
+      return NextResponse.json({ msj: 'Bien cargo', status : 200 });
+    } else {
+      return NextResponse.json({ msj: 'Mal no cargo', status : 404 });
     }
-    prismaClerk()
-
-    return NextResponse.json({ msj: 'Bien cargo', status : 200 })
-  } else {
-
-    return NextResponse.json({ msj: 'Mal no cargo', status : 404 })
+  } catch (error) {
+    console.error(error);
+    // Handle the error here
+    return NextResponse.json({ msj: 'Error', status : 500 });
   }
-
 };
